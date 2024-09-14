2a072bd391ad5fcc226619c3c55c29c0f2897600
Von Gong <vgong@batteriesplus.com>
2024-09-14 07:13:40 -0500

    Rewrite logic for Async Data pull

--- Finder.ps1
+++ Finder.ps1
@@ -1,9 +1,10 @@
 
+$fgDef = "Gray"
 $fgBold = "White"
 $fgGood = "Green"
 $fgWarn = "Yellow"
-# $fgErr = "Red"
-$fgDef = "Gray"
+$fgErr = "Red"
+$dateFormat = "dd MMM yyyy HH:mm"
 $SchedulerUriBase = "https://ttp.cbp.dhs.gov/schedulerapi/slot-availability?locationId={{Code}}"
 $SiteCodeFilePath = ".\data\SiteCodes.json"
 $histFileTemplate = ".\out\SiteCodesHist{{value}}.json"
@@ -14,13 +15,12 @@ Write-Host "Global Entry Appointment Finder" -ForegroundColor $fgBold
 Write-Host "Loading Site List"
 if (-not (Test-Path -Path $SiteCodeFilePath -PathType Leaf)) {
     $msg = "Can't find $SiteCodeFilePath"
-    Write-Error $msg
+    Write-Host $msg -ForegroundColor $fgErr
     throw $msg
 }
 $SiteCodes = Get-Content -Path $SiteCodeFilePath | ConvertFrom-Json -AsHashtable
+$SiteCodesPrior = $SiteCodes.Clone()
 
-# Make backup
-$SiteCodesPrior = @{}
 if (-not (Test-Path -Path (Split-Path $histFile) )) {
     Write-Host "Create folder for History"
     $msg = mkdir (Split-Path $histFile)
@@ -30,52 +30,53 @@ if (Test-Path -Path $histFile -PathType Leaf) {
     $SiteCodesPrior = Get-Content -Path $histFile | ConvertFrom-Json -AsHashtable
     Copy-Item -Path $histFile -Destination $histFile1 -Force
 }
-$SiteCodesHist = @{}
 
-Write-Host "Processing List" -ForegroundColor $fgBold
-foreach ($sitekey in $siteCodes.Keys) {
-    $siteCode = $SiteCodes[$sitekey]
-    $SchedulerUri = $SchedulerUriBase.Replace("{{Code}}",$siteCode)
+Write-Host "Pull Data"
+$SiteCodesCurr = $SiteCodes.Clone()
+$SiteCodes.Keys | Foreach-Object -ThrottleLimit 5 -Parallel {
+    $key = $_
+    $SiteCodes = $using:SiteCodes
+    $SiteCodesCurr = $using:SiteCodesCurr
+    $SchedulerUriBase = $using:SchedulerUriBase
+    $value = $SiteCodes[$key]
+    $SchedulerUri = $SchedulerUriBase.Replace("{{Code}}",$value)
+
     $resp = Invoke-WebRequest -Uri $SchedulerUri
     $rJson = $resp.Content | ConvertFrom-Json
+    $SiteCodesCurr[$key] = $rJson.availableSlots[0].startTimestamp
+}
+
+Write-Host "Process Data"
+foreach ($key in $SiteCodes.Keys) {
     $fgColor = $fgDef
     $fgSubColor = $fgDef
-    if ($rJson.availableSlots.Length -eq 0) {
+    if ($SiteCodesCurr[$key].Length -eq 0) {
         $msg = "No Available Slots"
     } else {
         $fgSubColor = $fgWarn
-        $dateStr = $rJson.availableSlots[0].startTimestamp
-        $SiteDate = [datetime]$rJson.availableSlots[0].startTimestamp
-        $msg = ([datetime]$dateStr).ToString('dd MMM yyyy HH:mm')
-        if ($SiteCodesPrior.ContainsKey($sitekey)) {
-            if ($null -eq $SiteCodesPrior[$sitekey]) {
+        $CurrDate = [datetime]$SiteCodesCurr[$key]
+        $msg = $CurrDate.ToString($dateFormat)
+        if ($SiteCodesPrior[$key] -ne $SiteCodesCurr[$key]) {
+            $PriorDate = [datetime]$SiteCodesPrior[$key]
+            $diffDate = New-TimeSpan -Start $PriorDate -End $CurrDate
+            if ($PriorDate -gt $CurrDate) {
                 $fgSubColor = $fgGood
-                $msg += " (Prev None)"
+                $fgColor = $fgBold
+            }
+            if ($diffDate.Days -ne 0) {
+                $msg += " ($($diffDate.Days) Days)"
+            } elseif ($diffDate.Hours -ne 0) {
+                $msg += " ($($diffDate.Hours) Hours)"
             } else {
-                $PriorDate = [datetime]$SiteCodesPrior[$sitekey]
-                if ($PriorDate -gt $SiteDate) {
-                    $fgSubColor = $fgGood
-                    $fgColor = $fgBold
-                }
-                if ($PriorDate -ne $SiteDate) {
-                    $diffDate = New-TimeSpan -Start $PriorDate -End $SiteDate
-                    # $msg += " ($($PriorDate.ToString('dd MMM yyyy'))"
-                    if ($diffDate.Days -ne 0) {
-                        $msg += " ($($diffDate.Days) Days)"
-                    } else {
-                        $msg += " ($($diffDate.Hours) Hours)"
-                    }
-                }
+                $msg += " ($($diffDate.Minutes) Min)"
             }
         }
     }
-    Write-Host "  $sitekey`: " -NoNewline -ForegroundColor $fgColor
-    Write-Host $msg -ForegroundColor $fgSubColor
-    $SiteCodesHist[$sitekey] = $rJson.availableSlots[0].startTimestamp
+    Write-Host "  $Key`: " -NoNewline -ForegroundColor $fgColor
+    Write-Host $msg  -ForegroundColor $fgSubColor
 }
 
 Write-Host "Writing History"
-Set-Content -Path $histFile -Value ($SiteCodesHist | ConvertTo-Json -Depth 10)
+Set-Content -Path $histFile -Value ($SiteCodesCurr | ConvertTo-Json -Depth 10)
 
 Write-Host "Complete"
-
